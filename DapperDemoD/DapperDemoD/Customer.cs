4031b0fc30e3b0b253c2b1881d61fa74fbd13e85
euroland3 <euroland3@EUROLAND3-PC>
2017-12-05 15:47:07 +0700

    Example Dapper

---
+++ DapperDemoD/DapperDemoD/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemoD
+{
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerfName { get; set; }
+        public string CustomerlName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- DapperDemoD/DapperDemoD/Program.cs
+++ DapperDemoD/DapperDemoD/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace DapperDemoD
 {
@@ -10,6 +14,19 @@ namespace DapperDemoD
     {
         static void Main(string[] args)
         {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefautlConnection"].ConnectionString);
+            string SqlString = "SELECT TOP 100 [CustomerID],[CustomerfName], [CustomerlName], [IsActive] FROM [Customer] ";
+            var ourCustomer = (List<Customer>)db.Query<Customer>(SqlString);
+            foreach (var Customer in ourCustomer)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nCustomerID: " + Customer.CustomerID.ToString());
+                Console.WriteLine("\nCustomerfName: " + Customer.CustomerfName);
+                Console.WriteLine("\nCustomerlName: " + Customer.CustomerlName);
+                Console.WriteLine("\nIsActive: " + Customer.IsActive+"\n");
+                Console.WriteLine(new string('*',20));
+            }
+            Console.ReadKey(true);
         }
     }
 }
